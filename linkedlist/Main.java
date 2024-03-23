b1b8b1e6069965aadc4e4c28645fe518be2f55bf
Rajeev-Ranjan <itrajeevranjan@gmail.com>
2024-03-24 01:27:09 +0530

    merge two list add questions

--- linkedlist/Main.java
+++ linkedlist/Main.java
@@ -3,14 +3,14 @@ package linkedlist;
 public class Main {
     public static void main(String[] args) {
 
-        LL list = new LL();
+//        LL list = new LL();
 //        list.insertFirst(17);
-        list.insertFirst(8);
-        list.insertFirst(2);
-        list.insertFirst(3);
-        list.display();
-
-        list.insertLast(155);
+//        list.insertFirst(8);
+//        list.insertFirst(2);
+//        list.insertFirst(3);
+//        list.display();
+//
+//        list.insertLast(155);
 //        list.display();
 //
 //        list.insert(1001, 2);
@@ -25,13 +25,27 @@ public class Main {
 //        System.out.println(list.find(1001));
 //        System.out.println(list.size);
 //
-        list.insertRec(88 , 2);
-        list.display();
+//        list.insertRec(88 , 2);
+//        list.display();
 //        list.insertRec(88 , 2);
 
 //        list.display();
 
-        System.out.println(list.getNthFromLast(1));
+//        System.out.println(list.getNthFromLast(1));
+        LL first = new LL();
+        LL second = new LL();
+        first.insertLast(1);
+        first.insertLast(2);
+        first.insertLast(4);
+
+        first.display();
+
+        second.insertLast(1);
+        second.insertLast(3);
+        second.insertLast(4);
+        second.display();
+
+
 
     }
 }
