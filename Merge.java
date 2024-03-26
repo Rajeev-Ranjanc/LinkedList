83962ffde030463275120f250f87a90f0bfbd669
Rajeev-Ranjanc <itrajeevranjan@gmail.com>
2024-03-27 01:45:48 +0530

    updation

--- Merge.java
+++ Merge.java
@@ -2,7 +2,7 @@ import linkedlist.LL;
 
 //https://leetcode.com/problems/merge-two-sorted-lists/merge-two-sorted-lists
 public class Merge {
-    private ListNode head;
+    ListNode head;
 
     public void insert(int val) {
         ListNode node = new ListNode(val);
@@ -29,9 +29,33 @@ public class Merge {
     }
 
 
-//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-//
-//    }
+    public static Merge merge(Merge first, Merge second) {
+        ListNode f = first.head;
+        ListNode s = second.head;
+
+        Merge ans = new Merge();
+
+
+        while (f != null && s != null) {
+            if (f.value < s.value) {
+                ans.insert(f.value);
+                f = f.next;
+            } else {
+                ans.insert(s.value);
+                s = s.next;
+            }
+        }
+
+        while (f != null) {
+            ans.insert(f.value);
+            f = f.next;
+        }
+        while (s != null) {
+            ans.insert(s.value);
+            s = s.next;
+        }
+        return ans;
+    }
 
     private class ListNode {
         private int value;
@@ -61,6 +85,8 @@ public class Merge {
         merge1.insert(3);
         merge1.insert(4);
         merge1.display();
+
+
     }
 
 
